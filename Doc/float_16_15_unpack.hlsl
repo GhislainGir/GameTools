3ac36a20d47a8e623d8b5cb61d14c5c683d8804a
Ghislain Girardot <ghislainatcremuss@gmail.com>
2025-04-25 12:50:31 +0200

    Update float_16_15_unpack.hlsl

--- Doc/float_16_15_unpack.hlsl
+++ Doc/float_16_15_unpack.hlsl
@@ -6,7 +6,7 @@ const uint packed_int = asuint(inXYFloat);
 // X
 // *
 
-// shift 15 bits to the left to isolate X component
+// shift 15 bits to the right to isolate X component
 uint packed_x = packed_int >> 15;
 //   XXXXXXXX0XXXXXXXXYYYYYYYYYYYYYYY
 // > 000000000000000XXXXXXXX0XXXXXXXX
@@ -16,19 +16,25 @@ uint packed_x = packed_int >> 15;
 //   000000000000000XXXXXXXX0XXXXXXXX
 //   000000000000000aaaaaaaa0bbbbbbbb
 
-// shift 9 bits to the left to isolate 'a'
+// shift 9 bits to the right to isolate 'a'
 uint packed_x_a = packed_x >> 9;
 //   000000000000000aaaaaaaa0bbbbbbbb
 // > 000000000000000000000000aaaaaaaa
-// mask 8 leftmost bits to isolate 'b'
+// shift 8 bits to the left
+packed_x_a = (packed_x_a << 8)
+//   000000000000000000000000aaaaaaaa
+// > 0000000000000000aaaaaaaa00000000
+// mask 8 rightmost bits to isolate 'b'
 uint packed_x_b = packed_x & ((1 << 8) - 1);
 //   000000000000000aaaaaaaa0bbbbbbbb
 // & 00000000000000000000000011111111
 // > 000000000000000000000000bbbbbbbb
 
-// shift 'a' 8 bits to the left and merge 'ab'
-packed_x = (packed_x_a << 8) | packed_x_b;
-//   0000000000000000aaaaaaaabbbbbbbb
+// merge 'ab'
+packed_x = packed_x_a | packed_x_b;
+//   0000000000000000aaaaaaaa00000000
+// | 000000000000000000000000bbbbbbbb
+// = 0000000000000000aaaaaaaabbbbbbbb
 
 // bring back extracted X to [0:1] range
 x = packed_x / float((1 << 16) - 1);
@@ -40,7 +46,7 @@ x += inMin.x;
 // Y
 //*
 
-// mask out the 16 leftmost bits to isolate Y
+// mask out the 16 rightmost bits to isolate Y
 uint packed_y = packed_int & ((1 << 15) - 1);
 //   XXXXXXXX0XXXXXXXXYYYYYYYYYYYYYYY
 // & 00000000000000000111111111111111
