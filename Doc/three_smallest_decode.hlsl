9360f436f54d5056e4f2a368ddeb74e337737c77
Ghislain Girardot <ghislainatcremuss@gmail.com>
2025-05-12 15:09:36 +0200

    Update three_smallest_decode.hlsl

--- Doc/three_smallest_decode.hlsl
+++ Doc/three_smallest_decode.hlsl
@@ -1,19 +1,19 @@
 // interpret the bits sent as a float as if they were describing an unsigned integer
-EncodedQuat = asuint(QuatFloat);
+const uint EncodedQuat = asuint(QuatFloat);
 
 // we received a 32-bit float encoded using 2 bits for the index (to reconstruct the largest component),
 // and 10 bits for each of the other components
-CompressionBits = 10;
+const uint CompressionBits = 10;
 
 // 10-bit integer range (1023), which is used to encode each component's value
-CompressionMask = (1 << CompressionBits) - 1;
+const uint CompressionMask = (1 << CompressionBits) - 1;
 
 // shift the first two bits to the left by 30 bits to isolate the index of the largest component and
 // read bits as a simple integer ranging from 0 to 3
-MaxAbsQuatComponentIndex = EncodedQuat >> 30;
+const uint MaxAbsQuatComponentIndex = EncodedQuat >> 30;
 
 // decode each of the xyz components by shifting their corresponding 10 bits to the left
-Quat = float4(1.0, 1.0, 1.0, 1.0); // initialize the quaternion with default values
+float4 Quat = float4(1.0, 1.0, 1.0, 1.0); // initialize the quaternion with default values
 Quat.x = float((EncodedQuat >> (CompressionBits * 2)) & CompressionMask); // extract x component
 Quat.y = float((EncodedQuat >> (CompressionBits * 1)) & CompressionMask); // extract y component
 Quat.z = float((EncodedQuat >> (CompressionBits * 0)) & CompressionMask); // extract z component
