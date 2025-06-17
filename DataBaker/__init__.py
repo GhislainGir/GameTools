5b5c85b5113d2a1adcf938b4ae63fd75a421b88d
Ghislain Girardot <ghislainatcremuss@gmail.com>
2025-06-17 16:19:24 +0200

    Delete DataBaker directory

--- DataBaker/__init__.py
+++
@@ -1,7 +0,0 @@
-# @TODO origin_obj might not be correctly applied
-# @TODO origin_obj matrix inverted in the wrong order?
-# @TODO implement axis order
-
-# @TODO vcol pos, report says remapping resquired no?
-
-# @TODO remap normal edit says its invalid at times?! validity isn't edited?!
