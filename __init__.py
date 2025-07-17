ee8a32a7679266bb4424509f0e4dd31fdf9bb79d
Cremuss <ghislainatcremuss@gmail.com>
2025-07-18 00:27:14 +0200

    updated bl_info

--- __init__.py
+++ __init__.py
@@ -14,10 +14,10 @@
 bl_info = {
     "name": "Data Baker",
     "author": "Ghislain GIRARDOT, Joshua BOGART, George VOGIATZIS (Gvgeo)",
-    "version": (1, 1),
-    "blender": (4, 0, 0),
-    "location": "View 3D -> Bake Data",
-    "description": "Enables you to bake all kind of data (pivots, axis, shapekey offset/normals, linear mask, spherical mask and more, into UVs or Vertex Color",
+    "version": (0, 3, 1),
+    "blender": (4, 3, 2),
+    "location": "View 3D -> Game Tools",
+    "description": "Toolset for baking animations and mesh data into textures using customizable layouts, UV maps, vertex colors, and normals",
     "warning": "",
     "doc_url": "",
     "tracker_url": "",
@@ -25,78 +25,13 @@ bl_info = {
 }
 
 import bpy
-from bpy.props import PointerProperty
-from bpy.types import PropertyGroup
 import os
 
 from . import auto_load
 
-# def get_writable_preset_path(subdir: str) -> str:
-#     """
-#     Return the first preset folder we have permission to write to, given a preset subdir, excluding the vscode_development
-
-#     :param subdir: preset sub directory to look for
-#     :return: folder path
-#     :rtype: str
-#     """
-#     for path in bpy.utils.preset_paths("operator"):
-#         target_dir = os.path.join(path, subdir)
-
-#         if 'vscode_development' in path:
-#             continue
-
-#         if not os.path.exists(target_dir):
-#             try:
-#                 os.makedirs(target_dir, exist_ok=True)
-#             except PermissionError:
-#                 continue
-
-#         try:
-#             test_file = os.path.join(target_dir, "temp_test.txt")
-#             with open(test_file, 'w') as f:
-#                 f.write("test")
-#             os.remove(test_file)
-#             return target_dir
-#         except (PermissionError, OSError):
-#             continue
-
-#     return None
-
-# def install_default_presets():
-#     """
-#     Copy .py preset files bundled with the extension to the user preset folder, if not already in there
-#     """
-
-#     preset_subdirs = [
-#         "gametools_databaker",
-#         "gametools_oatbaker",
-#         "gametools_objectattributes",
-#         "gametools_sdfbaker",
-#         "gametools_vatbaker",
-#         "gametools_batbaker",
-#     ]
-
-#     for preset_subdir in preset_subdirs:
-#         writable_path = get_writable_preset_path(preset_subdir)
-#         if writable_path is None:
-#             print("Warning: No writable preset path found.")
-#             return
-
-#         addon_preset_path = os.path.join(os.path.dirname(__file__), "presets", preset_subdir)
-#         if not os.path.exists(addon_preset_path):
-#             os.makedirs(addon_preset_path)
-
-#         for file in os.listdir(addon_preset_path):
-#             src = os.path.join(addon_preset_path, file)
-#             dst = os.path.join(writable_path, file)
-#             if not os.path.exists(dst):
-#                 shutil.copy2(src, dst)
-
 auto_load.init()
 
 def register():
-    #install_default_presets() # @NOTE legacy method
-
     auto_load.register()
 
     if register_preset_path := getattr(bpy.utils, "register_preset_path", None): # @TODO check
